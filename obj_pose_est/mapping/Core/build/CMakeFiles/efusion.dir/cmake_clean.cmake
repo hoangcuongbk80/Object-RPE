facbc5e15280e1db557d044d9069362908d583d7
aass <hoangcuongbk80@gmail.com>
2019-07-19 15:16:48 +0200

    initial commit

---
+++ obj_pose_est/mapping/Core/build/CMakeFiles/efusion.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_compile.dir/Cuda/cuda_compile_generated_reduce.cu.o"
+  "CMakeFiles/cuda_compile.dir/Cuda/cuda_compile_generated_cudafuncs.cu.o"
+  "CMakeFiles/efusion.dir/GPUTexture.cpp.o"
+  "CMakeFiles/efusion.dir/ElasticFusion.cpp.o"
+  "CMakeFiles/efusion.dir/Ferns.cpp.o"
+  "CMakeFiles/efusion.dir/Deformation.cpp.o"
+  "CMakeFiles/efusion.dir/IndexMap.cpp.o"
+  "CMakeFiles/efusion.dir/GlobalModel.cpp.o"
+  "CMakeFiles/efusion.dir/Utils/Resolution.cpp.o"
+  "CMakeFiles/efusion.dir/Utils/Parse.cpp.o"
+  "CMakeFiles/efusion.dir/Utils/WindowsExtras.cpp.o"
+  "CMakeFiles/efusion.dir/Utils/RGBDOdometry.cpp.o"
+  "CMakeFiles/efusion.dir/Utils/DeformationGraph.cpp.o"
+  "CMakeFiles/efusion.dir/Utils/Intrinsics.cpp.o"
+  "CMakeFiles/efusion.dir/Utils/CholeskyDecomp.cpp.o"
+  "CMakeFiles/efusion.dir/Shaders/Vertex.cpp.o"
+  "CMakeFiles/efusion.dir/Shaders/Resize.cpp.o"
+  "CMakeFiles/efusion.dir/Shaders/FillIn.cpp.o"
+  "CMakeFiles/efusion.dir/Shaders/ComputePack.cpp.o"
+  "CMakeFiles/efusion.dir/Shaders/FeedbackBuffer.cpp.o"
+  "CMakeFiles/efusion.dir/Cuda/containers/device_memory.cpp.o"
+  "libefusion.pdb"
+  "libefusion.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/efusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
