6246eb47f6872db900288aea8953686f20f75d46
Dinh Cuong Hoang <cghg@tsv-learning-desktop>
2019-09-23 11:39:45 +0200

    docker_run

--- DenseFusion/docker/docker_run.sh
+++ DenseFusion/docker/docker_run.sh
@@ -14,5 +14,5 @@ image_name=hoangcuongbk80/densefusion-gpu:latest
 
 nvidia-docker run --name densefusion -it --rm -v /home/aass/Hoang-Cuong/Seg_SLAM/DenseFusion:/densefusion -v /media/aass/783de628-b7ff-4217-8c96-7f3764de70d9/Warehouse_Dataset:/warehouse_dataset densefusion-gpu /bin/bash
 
-nvidia-docker run --name densefusion -it --rm -v /home/cghg/DenseFusion:/densefusion -v /media/DiskStation/trsv/data/YCB_Video_Dataset:/YCB_Video_Dataset hoangcuongbk80/densefusion-pytorch-1.0 /bin/bash
+nvidia-docker run --name densefusion -it --rm -v /home/cghg/DenseFusion:/densefusion -v /media/DiskStation/trsv/data/Warehouse_Dataset:/warehouse_dataset hoangcuongbk80/densefusion-pytorch-1.0 /bin/bash
 
